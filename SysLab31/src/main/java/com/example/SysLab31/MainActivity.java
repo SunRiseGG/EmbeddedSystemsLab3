a355e651ff05f80c655baef192f7d0371301f95c
SunRiseGG <SunRiseGC4@gmail.com>
2020-05-07 01:28:59 +0300

    Additional task 1 added

--- SysLab31/src/main/java/com/example/SysLab31/MainActivity.java
+++ SysLab31/src/main/java/com/example/SysLab31/MainActivity.java
@@ -13,6 +13,7 @@ import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText inputN;
+    EditText inputIt;
     TextView resultText;
 
     @Override
@@ -24,25 +25,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
       resultText = findViewById(R.id.result);
       inputN = findViewById(R.id.N_in);
+      inputIt = findViewById(R.id.It_in);
     }
 
     public void onClick(View v) {
       if(v.getId() == R.id.count_btn) {
         if (inputN.getText().toString().length() > 0) {
-          BigInteger n = new BigInteger(inputN.getText().toString());
-          Ferma instanceFerma = new Ferma();
-          BigInteger[] result = instanceFerma.factorization(n);
-          if (result[0] == null) {
-            Toast.makeText(this, "Runtime Error", Toast.LENGTH_LONG).show();
+          if (inputIt.getText().toString().length() > 0) {
+            BigInteger n = new BigInteger(inputN.getText().toString());
+            Integer it = Integer.parseInt(inputIt.getText().toString());
+            Ferma instanceFerma = new Ferma();
+            BigInteger[] result = instanceFerma.factorization(n, it);
+            if (result[0] == null) {
+              Toast.makeText(this, "Runtime Error", Toast.LENGTH_LONG).show();
+            }
+            String resultOutput = result[0] + ", " + result[1];
+            resultText.setText(resultOutput);
           }
-          String resultOutput = result[0] + ", " + result[1];
-          resultText.setText(resultOutput);
         }
       }
     }
 
-    private static class Ferma {
-      public BigInteger[] factorization(BigInteger n) {
+    public class FermaException extends Throwable {
+      public FermaException() {
+        super();
+      }
+
+      public FermaException(String message) {
+        super(message);
+      }
+    }
+
+    private class Ferma {
+      public BigInteger[] factorization(BigInteger n, Integer it) {
+        int iteration = 0;
         double startTime = System.currentTimeMillis(), timeNow;
         boolean endFlag = false;
         long x = (long) Math.sqrt(n.doubleValue());
@@ -56,10 +72,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             result[1] = BigInteger.valueOf(x + y);
           }
           x++;
+          iteration++;
           if (x > n.longValue()) {
             result[0] = BigInteger.valueOf(1);
             result[1] = n;
             endFlag = true;
+          }
+          try {
+            if (iteration > it) {
+              throw new FermaException("За задану кількість ітерацій результат не знайдено");
+            }
+          } catch(FermaException e) {
+            Toast error = Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT);
+            error.show();
+            e.printStackTrace();
           }
           timeNow = System.currentTimeMillis() - startTime;
           if (timeNow > 3000) {
